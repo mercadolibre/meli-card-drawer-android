edce9b66c71bb0570e3e67418b297cf2fc303c8d
Jorge <jorge.gonzalez@mercadolibre.com>
2020-03-09 12:00:47 -0300

    shadow no-dependency in screen density

--- carddrawer/src/main/java/com/meli/android/carddrawer/configuration/ShadowFontConfiguration.java
+++ carddrawer/src/main/java/com/meli/android/carddrawer/configuration/ShadowFontConfiguration.java
@@ -10,14 +10,16 @@ public abstract class ShadowFontConfiguration implements CardFontConfiguration {
 
     private final int shadowRadius;
     private final int shadowColor;
+    private final float shadowHeight;
 
     ShadowFontConfiguration(@NonNull Context context) {
         shadowRadius = context.getResources().getDimensionPixelSize(R.dimen.card_drawer_shadow_radius);
         shadowColor = ContextCompat.getColor(context, R.color.card_drawer_number_shadow_color);
+        shadowHeight = (context.getResources().getDisplayMetrics().density * 2) + 0.5f;
     }
 
     @Override
     public void setShadow(@NonNull final TextPaint textPaint) {
-        textPaint.setShadowLayer(shadowRadius, 0, 2, shadowColor);
+        textPaint.setShadowLayer(shadowRadius, 0, shadowHeight, shadowColor);
     }
 }
