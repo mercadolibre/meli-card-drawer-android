58c72ac7509a25cde270eda5a9c214c44a051614
Dani Villegas <daniel.dvillegas@mercadolibre.com>
2021-12-09 12:23:40 -0300

    fixed card high

--- app/src/main/java/com/meli/android/carddrawer/app/model/DebinConfiguration.kt
+++ app/src/main/java/com/meli/android/carddrawer/app/model/DebinConfiguration.kt
@@ -7,9 +7,10 @@ import com.meli.android.carddrawer.utils.DensityHelper
 
 class DebinConfiguration(context: Context) : GenericPaymentMethod(
     Color.BLUE,
-    Text("Banco Galicia", Color.parseColor("#FFFFFF")),
+    Text("Banco BBVA", Color.parseColor("#FFFFFF")),
     "https://http2.mlstatic.com/storage/logos-api-admin/227062e0-ae66-11eb-9123-2107040a9cba-xl@2x.png",
     Text("Cuenta corriente", Color.parseColor("#FFFFFF")),
     Tag("Novo", Color.parseColor("#1A4189E6"), Color.parseColor("#009EE3"), "semi_bold"),
-    Text("CBU: ***1234", Color.parseColor("#FFFFFF"))
+    Text("CBU: ***1234", Color.parseColor("#FFFFFF")),
+    arrayListOf("#002444", "#004580", "#0067BE")
 )
