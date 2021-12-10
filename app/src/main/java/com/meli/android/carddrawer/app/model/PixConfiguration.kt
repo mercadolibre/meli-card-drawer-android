875fcd3eb6fd50fdf8da0c25839eef21032a1bbe
Dani Villegas <daniel.dvillegas@mercadolibre.com>
2021-12-10 16:24:35 -0300

    fixed medium card

--- app/src/main/java/com/meli/android/carddrawer/app/model/PixConfiguration.kt
+++ app/src/main/java/com/meli/android/carddrawer/app/model/PixConfiguration.kt
@@ -6,11 +6,11 @@ import com.meli.android.carddrawer.model.GenericPaymentMethod
 import com.meli.android.carddrawer.utils.DensityHelper
 
 class PixConfiguration(context: Context) : GenericPaymentMethod(
-    Color.BLUE,
-    Text("PIX", Color.parseColor("#CC000000")),
+    Color.WHITE,
+    Text("PIX", Color.parseColor("#FFFFFF")),
     "https://http2.mlstatic.com/storage/logos-api-admin/227062e0-ae66-11eb-9123-2107040a9cba-xl@2x.png",
-    Text("Aprovação Imediata", Color.parseColor("#00A650")),
+    Text("Aprovação Imediata", Color.parseColor("#FFFFFF")),
     Tag("Novo", Color.parseColor("#1A4189E6"), Color.parseColor("#009EE3"), "semi_bold"),
     null,
-    arrayListOf("#132F3B", "#37B4AA", "#3688AB")
+    arrayListOf("#132F3B", "#132F3B", "#3688AB")
 )
