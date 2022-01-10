7ee9386e5bb4634d3dcb4ecdbef469a302f2f9a6
amaurirodrigues <ardsjj@gmail.com>
2022-01-10 16:05:07 -0300

    [PXN-2986] add tag tests

--- carddrawer/src/androidTest/java/com/meli/android/carddrawer/ViewScenarioRule.kt
+++ carddrawer/src/androidTest/java/com/meli/android/carddrawer/ViewScenarioRule.kt
@@ -1,5 +1,6 @@
 package com.meli.android.carddrawer
 
+import android.app.Activity
 import android.content.Context
 import android.view.View
 import android.view.ViewGroup
@@ -9,11 +10,14 @@ import org.junit.rules.ExternalResource
 
 class ViewScenarioRule : ExternalResource() {
 
+    lateinit var activity: Activity
+
     @Suppress("UNCHECKED_CAST")
     fun <V : View> launch(viewFactory: (Context) -> V, onView: ((V) -> Unit)? = null) {
-        ActivityScenario.launch(EmptyActivity::class.java).onActivity { activity ->
-            activity.setContentView(viewFactory(activity))
-            activity.findViewById<ViewGroup>(android.R.id.content).getChildAt(0).let {
+        ActivityScenario.launch(EmptyActivity::class.java).onActivity { emptyActivity ->
+            this.activity = emptyActivity
+            emptyActivity.setContentView(viewFactory(emptyActivity))
+            emptyActivity.findViewById<ViewGroup>(android.R.id.content).getChildAt(0).let {
                 onView?.invoke(it as V)
             }
         }
--- carddrawer/src/androidTest/java/com/meli/android/carddrawer/model/CardDrawerViewTest.kt
+++ carddrawer/src/androidTest/java/com/meli/android/carddrawer/model/CardDrawerViewTest.kt
@@ -1,13 +1,15 @@
 package com.meli.android.carddrawer.model
 
+import android.content.Context
+import android.graphics.Color
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.assertion.ViewAssertions.matches
-import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
-import androidx.test.espresso.matcher.ViewMatchers.withClassName
-import androidx.test.espresso.matcher.ViewMatchers.withId
-import androidx.test.espresso.matcher.ViewMatchers.withText
+import androidx.test.espresso.matcher.ViewMatchers.*
 import com.meli.android.carddrawer.R
 import com.meli.android.carddrawer.ViewScenarioRule
+import com.meli.android.carddrawer.configuration.DefaultCardConfiguration
+import io.mockk.every
+import io.mockk.mockk
 import org.hamcrest.Matchers.endsWith
 import org.junit.Rule
 import org.junit.jupiter.api.BeforeEach
@@ -21,44 +23,87 @@ class CardDrawerViewTest {
     val viewScenario = ViewScenarioRule()
 
     @Nested
-    @DisplayName("When is rendered without values")
-    inner class WhenIsRenderedWithDefaultValues {
+    @DisplayName("Given CardDrawerView with default configuration")
+    inner class GivenCardDrawerViewWithDefaultConfiguration {
+
+        lateinit var subject: CardDrawerView
 
         @BeforeEach
         fun setUp() {
             viewScenario.launch(
-                    viewFactory = { CardDrawerView(it) }
+                viewFactory = { CardDrawerView(it) },
+                onView = { subject = it }
             )
         }
 
-        @Test
-        @DisplayName("Then the CardDrawer is displayed")
-        fun thenTheCardDrawerIsDisplayed() {
-            onView(withClassName(endsWith("CardDrawerView"))).check(matches(isDisplayed()))
+        @Nested
+        @DisplayName("When is rendered with tag")
+        inner class WhenIsRenderedWithTag {
+
+            private val expectedTagText = "Sample Tag"
+            private val cardDrawerSource = mockk<PaymentCard>(relaxed = true)
+            private val tag = buildTag(expectedTagText, "regular")
+
+            @BeforeEach
+            fun setUp() {
+                every { cardDrawerSource.cardUI } returns buildConfiguration(viewScenario.activity)
+                every { cardDrawerSource.tag } returns tag
+                viewScenario.activity.runOnUiThread { subject.show(cardDrawerSource) }
+            }
+
+            @Test
+            @DisplayName("Then display tag with expected values")
+            fun thenDisplayTagWithExpectedValues() {
+                onView(withText(expectedTagText.toUpperCase())).check(matches(withId(R.id.card_tag)))
+            }
         }
 
-        @Test
-        @DisplayName("Then the card name placeholder is displayed")
-        fun thenTheCardNamePlaceholderIsDisplayed() {
-            onView(withId(R.id.cho_card_name)).check(matches(withText("Nombre y Apellido")))
-        }
+        @Nested
+        @DisplayName("When is rendered without values")
+        inner class WhenIsRenderedWithDefaultValues {
 
-        @Test
-        @DisplayName("Then the card expiration placeholder is displayed")
-        fun thenTheCardExpirationPlaceholderIsDisplayed() {
-            onView(withId(R.id.cho_card_date)).check(matches(withText("MM/AA")))
-        }
+            @Test
+            @DisplayName("Then the CardDrawer is displayed")
+            fun thenTheCardDrawerIsDisplayed() {
+                onView(withClassName(endsWith("CardDrawerView"))).check(matches(isDisplayed()))
+            }
 
-        @Test
-        @DisplayName("Then the card number truncated is displayed")
-        fun thenTheCardNumberTruncatedIsDisplayed() {
-            onView(withId(R.id.cho_card_number)).check(matches(withText("****  ****  ****  ****")))
-        }
+            @Test
+            @DisplayName("Then the card name placeholder is displayed")
+            fun thenTheCardNamePlaceholderIsDisplayed() {
+                onView(withId(R.id.cho_card_name)).check(matches(withText("Nombre y Apellido")))
+            }
 
-        @Test
-        @DisplayName("Then the card cvv truncated is displayed")
-        fun thenTheCardCVVTruncatedIsDisplayed() {
-            onView(withId(R.id.cho_card_code_front)).check(matches(withText("****")))
+            @Test
+            @DisplayName("Then the card expiration placeholder is displayed")
+            fun thenTheCardExpirationPlaceholderIsDisplayed() {
+                onView(withId(R.id.cho_card_date)).check(matches(withText("MM/AA")))
+            }
+
+            @Test
+            @DisplayName("Then the card number truncated is displayed")
+            fun thenTheCardNumberTruncatedIsDisplayed() {
+                onView(withId(R.id.cho_card_number)).check(matches(withText("****  ****  ****  ****")))
+            }
+
+            @Test
+            @DisplayName("Then the card cvv truncated is displayed")
+            fun thenTheCardCVVTruncatedIsDisplayed() {
+                onView(withId(R.id.cho_card_code_front)).check(matches(withText("****")))
+            }
         }
+    }
+
+    private fun buildConfiguration(context: Context): DefaultCardConfiguration {
+        return DefaultCardConfiguration(context)
+    }
+
+    private fun buildTag(text: String, weight: String): CardDrawerSource.Tag {
+        return CardDrawerSource.Tag(
+                text,
+                Color.parseColor("#CCCCCC"),
+                Color.parseColor("#FFFFFF"),
+                weight
+        )
     }
 }
