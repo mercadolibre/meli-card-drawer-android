9369853ff2f433b5d335b00315ac500f3b5f68b1
amaurirodrigues <ardsjj@gmail.com>
2022-01-11 17:22:02 -0300

    [PXN-2986] fix tests

--- carddrawer/src/androidTest/java/com/meli/android/carddrawer/model/CardDrawerViewTest.kt
+++ carddrawer/src/androidTest/java/com/meli/android/carddrawer/model/CardDrawerViewTest.kt
@@ -11,175 +11,121 @@ import com.meli.android.carddrawer.configuration.DefaultCardConfiguration
 import io.mockk.every
 import io.mockk.mockk
 import org.hamcrest.Matchers.endsWith
+import org.junit.Before
 import org.junit.Rule
-import org.junit.jupiter.api.BeforeEach
-import org.junit.jupiter.api.DisplayName
-import org.junit.jupiter.api.Nested
-import org.junit.jupiter.api.Test
+import org.junit.Test
 
 class CardDrawerViewTest {
 
     @get:Rule
     val viewScenario = ViewScenarioRule()
 
-    @Nested
-    @DisplayName("Given CardDrawerView with default configuration")
-    inner class GivenCardDrawerViewWithDefaultConfiguration {
+    private lateinit var subject: CardDrawerView
+    private val expectedCardName = "Maria Alessandra Louise Figueiredo"
+    private val expectedCardExpiration = "12/12"
+    private val cardNumber = "5325  4816  4243  7098"
+    private val expectedCardNumber = "5325  4816  4243  7098"
+    private val cvv = "11"
+    private val expectedCVV = "11**"
 
-        lateinit var subject: CardDrawerView
-        private val expectedCardName = "Nombre y Apellido"
-        private val expectedCardExpiration = "MM/AA"
-        private val expectedCardNumber = "****  ****  ****  ****"
-        private val expectedCVV = "****"
-
-        @BeforeEach
-        fun setUp() {
-            viewScenario.launch(
-                viewFactory = { CardDrawerView(it) },
-                onView = { subject = it }
-            )
-        }
-
-        @Nested
-        @DisplayName("When is rendered with tag")
-        inner class WhenIsRenderedWithTag {
-
-            private val expectedTagText = "Sample Tag"
-            private val cardDrawerSource = mockk<PaymentCard>(relaxed = true)
-            private val tag = buildTag(expectedTagText, "regular")
-
-            @BeforeEach
-            fun setUp() {
-                every { cardDrawerSource.cardUI } returns buildConfiguration(viewScenario.activity)
-                every { cardDrawerSource.tag } returns tag
-                viewScenario.activity.runOnUiThread { subject.show(cardDrawerSource) }
-            }
-
-            @Test
-            @DisplayName("Then display tag with expected values")
-            fun thenDisplayTagWithExpectedValues() {
-                onView(withText(expectedTagText.toUpperCase())).check(matches(withId(R.id.card_tag)))
-            }
-        }
-
-        @Nested
-        @DisplayName("When is rendered with bottom label")
-        inner class WhenIsRenderedWithBottomLabel {
-
-            private val expectedBottomLabel = "Sample bottom label"
-
-            @BeforeEach
-            fun setUp() {
-                val label = Label(
-                    text = expectedBottomLabel,
-                    color = "#000000",
-                    backgroundColor = "red"
-                )
-                subject.setBottomLabel(label)
-                subject.showBottomLabel()
-            }
-
-            @Test
-            @DisplayName("Then display tag with expected values")
-            fun thenDisplayTagWithExpectedValues() {
-                onView(withText(expectedBottomLabel))
-                    .check(matches(isDisplayed()))
-                    .check(matches(withId(R.id.card_drawer_bottom_description))
-                )
-            }
-        }
-
-        @Nested
-        @DisplayName("When is rendered without values")
-        inner class WhenIsRenderedWithDefaultValues {
-
-            @Test
-            @DisplayName("Then the CardDrawer is displayed")
-            fun thenTheCardDrawerIsDisplayed() {
-                onView(withClassName(endsWith("CardDrawerView"))).check(matches(isDisplayed()))
-            }
-
-            @Test
-            @DisplayName("Then the card name placeholder is displayed")
-            fun thenTheCardNamePlaceholderIsDisplayed() {
-                onView(withId(R.id.cho_card_name)).check(matches(withText(expectedCardName)))
-            }
-
-            @Test
-            @DisplayName("Then the card expiration placeholder is displayed")
-            fun thenTheCardExpirationPlaceholderIsDisplayed() {
-                onView(withId(R.id.cho_card_date)).check(matches(withText(expectedCardExpiration)))
-            }
-
-            @Test
-            @DisplayName("Then the card number truncated is displayed")
-            fun thenTheCardNumberTruncatedIsDisplayed() {
-                onView(withId(R.id.cho_card_number)).check(matches(withText(expectedCardNumber)))
-            }
-
-            @Test
-            @DisplayName("Then the card cvv truncated is displayed")
-            fun thenTheCardCVVTruncatedIsDisplayed() {
-                onView(withId(R.id.cho_card_code_front)).check(matches(withText(expectedCVV)))
-            }
-        }
+    @Before
+    fun setUp() {
+        viewScenario.launch(
+            viewFactory = { CardDrawerView(it) },
+            onView = { subject = it }
+        )
     }
 
-    @Nested
-    @DisplayName("Given CardDrawerView with user data")
-    inner class GivenCardDrawerViewWithUserData {
+    @Test
+    fun when_is_rendered_with_tag_then_display_tag_with_expected_values() {
 
-        private val expectedCardName = "Maria Alessandra Louise Figueiredo"
-        private val expectedCardExpiration = "12/12"
-        private val cardNumber = "5325  4816  4243  7098"
-        private val expectedCardNumber = "5325  4816  4243  7098"
-        private val cvv = "11"
-        private val expectedCVV = "11**"
+        val expectedTagText = "Sample Tag"
+        val cardDrawerSource = mockk<PaymentCard>(relaxed = true)
+        val tag = buildTag(expectedTagText, "regular")
 
-        @BeforeEach
-        fun setUp() {
-            viewScenario.launch(
-                viewFactory = { CardDrawerView(it) },
-                onView = {
-                    it.card = Card().apply {
-                        name = expectedCardName
-                        expiration = expectedCardExpiration
-                        number = cardNumber
-                        secCode = cvv
-                    }
-                    it.updateCardInformation()
-                }
-            )
+        every { cardDrawerSource.cardUI } returns buildConfiguration(viewScenario.activity)
+        every { cardDrawerSource.tag } returns tag
+        viewScenario.activity.runOnUiThread { subject.show(cardDrawerSource) }
+
+        onView(withText(expectedTagText.toUpperCase())).check(matches(withId(R.id.card_tag)))
+    }
+
+    @Test
+    fun when_is_rendered_with_bottom_label_then_display_tag_with_expected_values() {
+
+        val expectedBottomLabel = "Sample bottom label"
+        val label = Label(text = expectedBottomLabel,color = "#000000",backgroundColor = "red")
+
+        subject.setBottomLabel(label)
+        subject.showBottomLabel()
+
+        onView(withText(expectedBottomLabel))
+            .check(matches(isDisplayed()))
+            .check(matches(withId(R.id.card_drawer_bottom_description))
+        )
+    }
+
+    @Test
+    fun when_is_rendered_with_default_values_then_CardDrawerView_isDisplayed() {
+        onView(withClassName(endsWith("CardDrawerView"))).check(matches(isDisplayed()))
+    }
+
+    @Test
+    fun when_is_rendered_with_default_values_then_card_name_placeholder_isDisplayed() {
+        val expectedCardName = "Nombre y Apellido"
+        onView(withId(R.id.cho_card_name)).check(matches(withText(expectedCardName)))
+    }
+
+    @Test
+    fun when_is_rendered_with_default_values_then_card_expiration_placeholder_isDisplayed() {
+        val expectedCardExpiration = "MM/AA"
+        onView(withId(R.id.cho_card_date)).check(matches(withText(expectedCardExpiration)))
+    }
+
+    @Test
+    fun when_is_rendered_with_default_values_then_card_number_truncated_isDisplayed() {
+        val expectedCardNumber = "****  ****  ****  ****"
+        onView(withId(R.id.cho_card_number)).check(matches(withText(expectedCardNumber)))
+    }
+
+    @Test
+    fun when_is_rendered_with_default_values_then_card_CVV_truncated_isDisplayed() {
+        val expectedCVV = "****"
+        onView(withId(R.id.cho_card_code_front)).check(matches(withText(expectedCVV)))
+    }
+
+    @Test
+    fun when_is_rendered_with_user_values_then_card_name_placeholder_isDisplayed() {
+        updateCardInformation()
+        onView(withId(R.id.cho_card_name)).check(matches(withText(expectedCardName)))
+    }
+
+    @Test
+    fun when_is_rendered_with_user_values_then_card_expiration_placeholder_isDisplayed() {
+        updateCardInformation()
+        onView(withId(R.id.cho_card_date)).check(matches(withText(expectedCardExpiration)))
+    }
+
+    @Test
+    fun when_is_rendered_with_user_values_then_card_number_truncated_isDisplayed() {
+        updateCardInformation()
+        onView(withId(R.id.cho_card_number)).check(matches(withText(expectedCardNumber)))
+    }
+
+    @Test
+    fun when_is_rendered_with_user_values_then_card_CVV_Truncated_isDisplayed() {
+        updateCardInformation()
+        onView(withId(R.id.cho_card_code_front)).check(matches(withText(expectedCVV)))
+    }
+
+    private fun updateCardInformation() {
+        subject.card = Card().apply {
+            name = expectedCardName
+            expiration = expectedCardExpiration
+            number = cardNumber
+            secCode = cvv
         }
-
-        @Nested
-        @DisplayName("When is rendered with values")
-        inner class WhenIsRenderedWithValues {
-
-            @Test
-            @DisplayName("Then the card name is displayed")
-            fun thenTheCardNamePlaceholderIsDisplayed() {
-                onView(withId(R.id.cho_card_name)).check(matches(withText(expectedCardName)))
-            }
-
-            @Test
-            @DisplayName("Then the card expiration is displayed")
-            fun thenTheCardExpirationPlaceholderIsDisplayed() {
-                onView(withId(R.id.cho_card_date)).check(matches(withText(expectedCardExpiration)))
-            }
-
-            @Test
-            @DisplayName("Then the card number is displayed")
-            fun thenTheCardNumberTruncatedIsDisplayed() {
-                onView(withId(R.id.cho_card_number)).check(matches(withText(expectedCardNumber)))
-            }
-
-            @Test
-            @DisplayName("Then the card cvv truncated is displayed")
-            fun thenTheCardCVVTruncatedIsDisplayed() {
-                onView(withId(R.id.cho_card_code_front)).check(matches(withText(expectedCVV)))
-            }
-        }
+        subject.updateCardInformation()
     }
 
     private fun buildConfiguration(context: Context): DefaultCardConfiguration {
