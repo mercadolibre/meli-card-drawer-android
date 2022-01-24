b5aa572268f60a806ef9bf99fda2bf32f04e2b0a
Samanta Albanez <samanta.albanez@mercadolivre.com>
2022-01-24 17:24:01 -0300

    Change class from Java to Kotlin

--- carddrawer/src/test/java/com/meli/android/carddrawer/BasicRobolectricTest.kt
+++ carddrawer/src/test/java/com/meli/android/carddrawer/BasicRobolectricTest.kt
@@ -1,11 +1,13 @@
-package com.meli.android.carddrawer;
+package com.meli.android.carddrawer
 
-import android.content.Context;
+import android.content.Context
+import android.os.Build
+import androidx.test.core.app.ApplicationProvider
+import org.robolectric.RuntimeEnvironment
+import org.robolectric.annotation.Config
 
-import org.robolectric.RuntimeEnvironment;
-
-public class BasicRobolectricTest {
-    protected Context getContext() {
-        return RuntimeEnvironment.application;
-    }
+@Config(sdk = [Build.VERSION_CODES.LOLLIPOP_MR1])
+open class BasicRobolectricTest {
+    protected val context: Context
+        protected get() = ApplicationProvider.getApplicationContext()
 }
