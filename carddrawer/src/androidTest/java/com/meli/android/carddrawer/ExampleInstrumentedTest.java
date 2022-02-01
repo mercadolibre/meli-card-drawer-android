13d4820d90362bb984c929ffa4e5cc1fdde9bf88
Dani Villegas <daniel.dvillegas@mercadolibre.com>
2022-02-01 19:00:34 -0300

    fixed tests

--- carddrawer/src/androidTest/java/com/meli/android/carddrawer/ExampleInstrumentedTest.java
+++ carddrawer/src/androidTest/java/com/meli/android/carddrawer/ExampleInstrumentedTest.java
@@ -1,7 +1,8 @@
 package com.meli.android.carddrawer;
 
 import android.content.Context;
-import android.support.test.InstrumentationRegistry;
+
+import androidx.test.InstrumentationRegistry;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 
 import org.junit.Test;
