90023bbe1347f17f9f53d90bbdb2e9fa29d7ecb1
Dani Villegas <daniel.dvillegas@mercadolibre.com>
2022-02-02 22:12:57 -0300

    change pix image

--- app/src/main/java/com/meli/android/carddrawer/app/model/PixConfiguration.kt
+++ app/src/main/java/com/meli/android/carddrawer/app/model/PixConfiguration.kt
@@ -7,7 +7,7 @@ import com.meli.android.carddrawer.model.GenericPaymentMethod
 class PixConfiguration(context: Context) : GenericPaymentMethod(
     Color.WHITE,
     Text("PIX", Color.parseColor("#FFFFFF"), "semi_bold"),
-    "https://mobile.mercadolibre.com/remote_resources/image/card_drawer_mlb_pm_pix_normal?density=xxxhdpi&locale=en&version=1",
+    "https://http2.mlstatic.com/storage/logos-api-admin/8a135600-1fa5-11eb-b2cc-3f20f2f5ffe3-xl@2x.png",
     Text("Aprovação Imediata", Color.parseColor("#FFFFFF"), "regular"),
     Tag("Novo", Color.parseColor("#FFFFFF"), Color.parseColor("#009EE3"), "semi_bold"),
     arrayListOf("#132F3B", "#255E76", "#3688AB")
